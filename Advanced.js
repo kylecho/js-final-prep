214b77a9e4de64f87713afddaa45e7d85ad180ab
Kyle Cho <kylecho.work@gmail.com>
2015-05-22 14:26:12 -0700

    Complete Advanced.js

--- Advanced.js
+++ Advanced.js
@@ -1,1 +1,214 @@
 // Advanced js
+Defining Functions
+======================================================================================================
+// #5: What ways can we define functions?
+function isNimble(){ return true; } 
+var canFly = function(){ return true; }; 
+window.isDeadly = function(){ return true; }; 
+
+// #6: Does the order of function definition matter?
+var canFly = function(){ return true; }; 
+window.isDeadly = function(){ return true; }; 
+assert( isNimble() && canFly() && isDeadly(), "Still works, even though isNimble is moved." ); // pass
+function isNimble(){ return true; }
+
+// #7: Where can assignments be accessed?
+assert( typeof canFly == "undefined", "canFly doesn't get that benefit." ); // pass
+assert( typeof isDeadly == "undefined", "Nor does isDeadly." ); // pass
+var canFly = function(){ return true; }; 
+window.isDeadly = function(){ return true; };
+
+// #8: Can functions be defined below return statements?
+function stealthCheck(){ 
+  assert( stealth(), "We'll never get below the return, but that's OK!" ); // pass
+  return stealth();
+  function stealth(){ return true; } // it will be hoisted to the top of the current scope.
+} 
+stealthCheck();
+
+
+Named Functions
+======================================================================================================
+// #10: We can refer to a function, within itself, by its name.
+function yell(n){ 
+  return n > 0 ? yell(n-1) + "a" : "hiy"; 
+} 
+assert( yell(4) == "hiyaaaa", "Calling the function by itself comes naturally." ); // pass
+
+// #11: What is the name of a function?
+var ninja = function myNinja(){ 
+  assert( ninja == myNinja, "This function is named two things - at once!" ); // pass
+}; 
+ninja(); 
+assert( typeof myNinja == "undefined", "But myNinja isn't defined outside of the function." ); // pass
+log( ninja ); // function myNinja(){ assert( ninja == myNinja, "This function is named two things - at once!" ); }
+
+// #12: We can even do it if we're an anonymous function that's an object property. (Noteworthy *****)
+var ninja = { 
+  yell: function(n){ 
+    return n > 0 ? ninja.yell(n-1) + "a" : "hiy"; 
+  } 
+}; 
+assert( ninja.yell(4) == "hiyaaaa", "A single object isn't too bad, either." ); // pass
+
+// #13: But what happens when we remove the original object?
+var ninja = { 
+  yell: function(n){ 
+    return n > 0 ? ninja.yell(n-1) + "a" : "hiy"; 
+  } 
+}; 
+assert( ninja.yell(4) == "hiyaaaa", "A single object isn't too bad, either." ); // pass
+ 
+var samurai = { yell: ninja.yell }; 
+var ninja = null; 
+ 
+try { 
+  samurai.yell(4); 
+} catch(e){ 
+  assert( false, "Uh, this isn't good! Where'd ninja.yell go?" ); // fail, because ninja is null.
+}
+
+// #14: Let's give the anonymous function a name!
+var ninja = { 
+  yell: function yell(n){ // just gave an anonymous function a name. 'yell'
+    return n > 0 ? yell(n-1) + "a" : "hiy"; 
+  } 
+}; 
+assert( ninja.yell(4) == "hiyaaaa", "Works as we would expect it to!" ); // pass
+ 
+var samurai = { yell: ninja.yell }; // works this time. But why? (Difficulty *******)
+var ninja = null;
+assert( samurai.yell(4) == "hiyaaaa", "The method correctly calls itself." ); // pass but why?
+
+// #15: What if we don't want to give the function a name?
+var ninja = { 
+  yell: function(n){ 
+    return n > 0 ? arguments.callee(n-1) + "a" : "hiy"; // arguments.callee is the function itself.
+  } 
+}; 
+assert( ninja.yell(4) == "hiyaaaa", "arguments.callee is the function itself." ); // pass
+
+
+Functions as Objects (Noteworthy *****)
+======================================================================================================
+// #17: How similar are functions and objects?
+var obj = {};
+var fn = function(){};
+assert( obj && fn, "Both the object and function exists." ); // pass
+
+// #18: How similar are functions and objects?
+var obj = {};
+var fn = function(){};
+obj.prop = "some value";
+fn.prop = "some value"; // functions can have properties. (Noteworthy ****)
+asser( obj.prop == fn.prop, "Both are objects, both have the property." ); // pass
+
+// #19: Is it possible to cache the return results from a function? - yes
+function getElements( name ) {
+	var results;
+
+	if ( getElements.cache[name] ) { // cache property with "name" property exists? 
+		results = getElements.cache[name];
+	} else {
+		results = document.getElementsByTagName(name);
+		getElements.cache[name] = results; // if not, assign it with the value in results.
+	}
+
+	return results;
+}
+getElements.cache = {}; // cache object needs to be initiated.
+
+// #20: QUIZ: Can you cache the results of this function?
+function isPrime( num ) {
+	var prime = num != 1 // Everything but 1 can be prime
+	for ( var i = 2; i < num; i++ ) {
+		if ( num % i == 0 ) {
+			prime = false;
+			break;
+		}
+	}
+	return prime;
+}
+
+assert( isPrime(5), "Make sure the function works, 5 is prime" ); // pass
+assert( isPrime.cache[5], "Is the answer cached?" ); // ERROR Cannot read property of '5' of undefined
+
+// #21: One possible way to cache the results: (Noteworthy *******)
+function isPrime( num ) {
+	if ( isPrime.cache[ num ] != null )
+		return isPrime.cache[ num ]; // Very useful. Heavy calculation can be cached in a function.
+
+	var prime = num != 1 // Everything but 1 can be prime
+	for ( var i = 2; i < num; i++ ) {
+		if ( num % i == 0 ) {
+			prime = false;
+			break;
+		}
+	}
+	
+	isPrime.cache[ num ] = prime;
+
+	return prime;
+}
+
+isPrime.cache = {}; // However, cache needs to be initiated.
+
+assert( isPrime(5), "Make sure the function works, 5 is prime." ); // pass
+assert( isPrime.cache[5], "Make sure the answer is cached." ); // pass
+
+
+Context (Noteworthy *****)
+======================================================================================================
+// #23: What happens if a function is an object property?
+var katana = {
+	isSharp: true,
+	use: function(){
+		this.isSharp = !this.isSharp;
+	}
+};
+katana.use();
+assert( !katana.isSharp, "Veryfy the value of isSharp has been changed." ); // pass
+
+// #24: What exactly does context represent?
+function katana(){
+	this.isSharp = true; // this will point window object (Noteworthy *****)
+}
+katana();
+assert( isSharp === true, "A global object now exists with that name and value." ); // pass
+
+var shuriken = {
+	toss: function(){
+		this.isSharp.true; // this 'this' will point shuriken upon an invocation
+	}
+};
+shuriken.toss();
+assert( shuriken.isSharp === true, "When it's an object property, the value is set within the object." ); // pass
+
+// #25: How can we change the context of a function? (Noteworthy *****)
+var object = {};
+function fn(){
+	return this;
+}
+assert( fn() == this, "The context is the global object." ); // pass
+assert( fn.call(object) == object, "The context is changed to a specific object." ); // pass
+
+// #26: Different ways of changing the context: (Noteworthy *****)
+finction add(a, b){
+	return a + b;
+}
+assert( add.call(this, 1, 2) == 3, ".call() takes individual arguments" ); // pass
+assert( add.apply(this, [1, 2]) == 3, ".apply() takes an array of arguments" ); // pass
+
+// #27: QUIZ: How can we implement looping with a callback? (Noteworthy *******)
+function loop(array, fn){
+	for ( var i = 0; i < array.length; i++ ) {
+		fn.call( array, array[i], i ); // ?
+	}
+}
+var num = 0;
+loop([0, 1, 2], function(value, i){
+	assert(value == num++, "Make sure the contents are as we expect it.");
+	assert(this instanceof Array, "The context should be the full array.");
+});
+
+// To be continuedd
--- tricky-js-fundamentals.js
+++ tricky-js-fundamentals.js
@@ -1,7 +1,5 @@
-//Q
-//Easy stuff right? How about making a function that takes
-//two numbers, a base and exponent and returns the result
-=============================================================
+// 1. Making a function that takes two numbers, a base and exponent and returns the result
+==============================================================================================================
 var power = function(base, exponent) {
 	var result = base;
 	for (var i = 2; i <= exponent; i++) {
@@ -10,30 +8,39 @@ var power = function(base, exponent) {
 	return result;
 };
 
-//This function will return a number
-//[input: 2, 5]
-//[output: 32]
+// This function will return a number
+// [input: 2, 5]
+// [output: 32]
+// This is same as:
+Math.pow(base, power);
 
-//Q
-//range([start], stop, [step])
-//start, if omitted, defaults to 0; step defaults to 1.
-//Returns a list of integers from start to stop, incremented (or decremented) by step, exclusive.
-//Note that ranges that stop before they start are considered to be zero-length instead of negative.
-=============================================================
+// 2. range([start], stop, [step])
+// start, if omitted, defaults to 0; step defaults to 1. Returns a list of integers from start to stop, incremented
+// (or decremented) by step, exclusive. Note that ranges that stop before they start are considered to be zero-length
+// instead of negative.
+==============================================================================================================
 
-//Q
-//write a function called applied that mimics the
-//functionality of apply (without the context part)
-=============================================================
 
+// 3. Write a function called applied that mimics the functionality of apply (without the context part)
+==============================================================================================================
+var applied = function(func, array) {
+  return function(){
+    return func.apply(null, array);
+  };
+};
 //applied(func, [arr]) => func(arr1,arr2,arr3)
 
-//Q
-//write a function called partial that mimics this 
-//behavior //TODO: more details
-=============================================================
+// 4. Write a function called partial that mimics this (Difficulty *****)
+==============================================================================================================
 var fullName = function ( firstName, lastName ) {
   return firstName + ' ' + lastName;
+};
+
+var partial = function(func, arg) {
+  return function(x){
+    var myArgs = [arg, x];
+    return func.apply(null, myArgs);
+  };
 };
 
 var billName = partial( fullName, 'Bill' );
@@ -41,12 +48,9 @@ var billName = partial( fullName, 'Bill' );
 billName( 'Smith' ); // "Bill Smith"
 billName( 'Clinton' ); // "Bill Clinton"
 
-//Q (EXTRA CREDIT)
-//compose(fn1, fn2, ..., fnN)
-//Compose multiple functions to create a new function. 
-//For example, wu.compose(f, g, h)(x) is equivalent 
-//to f(g(h(x))).
-=============================================================
+// 5. (EXTRA CREDIT) compose(fn1, fn2, ..., fnN) Compose multiple functions to create a new function. 
+// For example, wu.compose(f, g, h)(x) is equivalent to f(g(h(x))). (Difficulty ******)
+==============================================================================================================
 var compose = function(f, g) {
   return function(x) {
     return f(g(x));
@@ -60,47 +64,36 @@ var compose = function(f, g) {
 >>> wu.compose(add2, square)(5);
 27
 
-//Q
-//write a function that will merge the first object into
-//the second object, using _.each. If the property already exists, on the second
-//object, do not overwrite it. 
-======================================================================
+// 6. Write a function that will merge the first object into the second object, using _.each. If the property 
+// already exists, on the second object, do not overwrite it. (Noteworthy *****)
+==============================================================================================================
 var merge = function(obj1, obj2) {
   var result = {};
-  _.each(obj1, function(value, key){
+  _.each(obj2, function(value, key){
     result[key] = value;
   });
-  _.each(obj2, function(value, key){
+  _.each(obj1, function(value, key){
+    if (!(key in obj2)) // checking is required.
     result[key] = value;
   });
   return result;
 };
 
 var obj1 = {one: 1, two: 2, three: 3, four: 4, five: 5},
-    obj2 = {three: 4, four: 5, five: 6};
-merge(obj1, obj2); // {one: 1, two: 2, three: 4, four: 5, five: 6}
+    obj2 = {three: 'a', four: 5, five: 6};
+merge(obj1, obj2); // {three: 'a', four: 5, five: 6, one: 1, two: 2}
 
-//Q
-//write a function that takes a nested array and returns
-//a one dimentional array.
-======================================================================
+
+// 7. Write a function that takes a nested array and returns a one dimentional array.
+==============================================================================================================
 var flatten = function(arrays) {
   return _.reduce(arrays, function(prev, curr) { // should return _.reduce()!!
     return prev.concat(curr); 
   }, []); // initial value of [] is optional
 };
 
-//Q
-//use _.map to change the 'name' property to your name and the 
-//'isAwesome' property to true of these objects in this array of 
-//objects -
-//[
-//{loves: 'iceCream' name: 'Billy', isAwesome: false;}
-//{loves: 'sandwiches' name: 'Albrey', isAwesome: false;}
-//{loves: 'pizza' name: 'Spencer', isAwesome: false;}
-//{loves: 'strawberries' name: 'Fred', isAwesome: false;}
-//]
-======================================================================
+// 8. Use _.map to change the 'name' property to your name and the 'isAwesome' property to true.
+==============================================================================================================
 var objects = [
 {loves: 'iceCream', name: 'Billy', isAwesome: false},
 {loves: 'sandwiches', name: 'Albrey', isAwesome: false},
@@ -117,6 +110,67 @@ _.map(objects, function(object) { // returns an array with applied elements
   });
 });
 
-//Q
-//last questions from Day6Challenges.js
-======================================================================
+// 9. Merge (Difficulty *****)
+==============================================================================================================
+var merge = function(array1, array2, callback) {
+  var result = [];
+  for (var i = 0; i < array1.length; i++) {
+    result.push(callback(array1[i], array2[i]));
+  }
+  return result;
+};
+
+var euclid = function(coords1, coords2) {
+  var myCoords = merge(coords1, coords2, function(a, b){
+    return a - b;
+  });
+  return Math.sqrt(Math.pow(myCoords[0], 2) + Math.pow(myCoords[1], 2)); // Use Math.pow(base, power)
+};
+
+var x = euclid([1.2, 3.67], [2.0, 4.4]);
+// x should now equal approximately 1.08
+euclid([1.2, 3.67], [2.0, 4.4]);
+1.7320508075688772
+
+// 9. Write a function that takes a random number(1-10) of randomly generated num inputs (1-10). With those 
+// inputs, you add them all together and return the result. randomAdder(1,4,7,2) => 14
+==============================================================================================================
+//generate the random numbers by using this function:
+var generateNum = function(){
+  return Math.floor(Math.random() * 11);
+};
+
+var randomAdder = function(times) {
+  var total = 0;
+  for (var i = 1; i <= times; i++) {
+    total += generateNum();
+  }
+  return total;
+};
+
+// 10. [EXTRA CREDIT] Why doesn`t the code below work? This is a function that should return an array of functions
+// that console.log() each person`s name as a string when invoked. Fiddle with this function and inspect how it works,
+// then try to fix it using a closure. Be prepared to explain to a partner how it worked before, and how it works
+// now with a closure. (Noteworthy ******)
+==============================================================================================================
+// Wrong code
+  var checkAttendanceFunc = function(nameArr){
+    var resultArr = [];
+    for(var i = 0; i < nameArr.length; i++){
+      resultArr.push(function(){ console.log('Is', nameArr[i], 'present?', i)}) // it's pushing a function definition.
+    };
+    return resultArr;
+  };
+
+// My answer
+  var checkAttendanceFunc = function(nameArr){
+    var resultArr = [];
+    var checker = function(arg){ 
+      console.log('Is', arg, 'present?', i);
+      return 'Is ' + arg + ' present? ' + i; // should have a return value to be saved.
+    };
+    for(var i = 0; i < nameArr.length; i++){
+      resultArr.push(checker(nameArr[i])); // should be evaluated. (i.e. invoked before being pushed.)
+    };
+    return resultArr;
+  };
