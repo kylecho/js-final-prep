7e3f5378450843ed1f502a056f193dd5b1787b27
Kyle Cho <kylecho.work@gmail.com>
2015-05-25 21:07:50 -0700

    Update final-recap.js

--- final-recap.js
+++ final-recap.js
@@ -8,15 +8,14 @@ false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should retur
 negative numbers.
 ==============================================================================================================
 var arrayAddition = function(array) {
-	var myArr = array.slice().sort(function(a, b){ return a - b; }); // adding .pop() will become largest.
-	var largest = myArr.shift(); // this is getting the smallest. 
+	var largest = array.slice().sort(function(a, b){ return a - b; }).pop();
 
 	var recursion = function recursion(target, array) {
 		if (array.length === 0) {
 			return target === 0;
 		} else {
 			var deduct = array.shift(); // wrong it should have been -> array[0];
-			// array = array.slice(1); // is missing
+			// array = array.slice(1); // was missing
 			return recursion(target - deduct, array) || recursion(target, array);
 		}
 	};
@@ -47,27 +46,28 @@ function MeanMode(arr) {
   var sum = 0;
   var count = 0;
   
-  for ( var i = 0; i < arr.length; i++ ) {
+  for ( var i = 0; i < arr.length; i++ ) { // buid up sum and modeCount simultaneously.
     sum += arr[i];
-    modeCount[arr[i]] = modeCount[arr[i]] || 0;
+    modeCount[arr[i]] = modeCount[arr[i]] || 0; // if existing, use it as it is. Otherwise set 0.
     modeCount[arr[i]] += 1;          //adding for the count of the number
   }
   mean = sum/arr.length;
-  for( var key in modeCount ){
-    if( modeCount[key] > count){     //**check the number of times a number has been seen
-      mode = parseInt(key);          //if its a new highest, convert the key back into a number and set it as the current mode
-      count = modeCount[key];        //set a new high count
+
+  for( var key in modeCount ){			 // finding mode is a separate for in loop.
+    if( modeCount[key] > count){     // **check the number of times a number has been seen
+      mode = parseInt(key);          // if its a new highest, convert the key back into a number and set it as the current mode
+      count = modeCount[key];        // set a new high count
     }
   }
 
-  return mode === mean ? 1 : 0;      //check to see if the mode and mean are equal
+  return mode === mean ? 1 : 0;      // check to see if the mode and mean are equal
 }
 
 // Time: 9 min
 // Expected result: 1
 // Actual result  : 0
 // Do I know it?: No.
-// Why was it wrong?: Finding meand and mode doesn't need to be a function.
+// Why was it wrong?: Finding mean and mode don't need to be a function.
 
 
 // 3. Reduce (Difficulty *****)
@@ -104,23 +104,7 @@ For example i. Also, always watch the name of original parameters for a function
 all the way, it should not be named array or collection. Silly but important to note.
 
 
-// 4. Pluck (Difficulty *****)
-A convenient version of what is perhaps the most common use-case for map: extracting a list of property values.
-==============================================================================================================
-var pluck = function(list, propertyName) {
-	return _.map(list, function(elem){
-		return elem[propertyName];
-	});
-};
-
-// Time: 4 min
-// Expected result: n/a
-// Actual result  : n/a
-// Do I know it?: No.
-// Why was it wrong?: I used each, where I should have used map.
-
-
-// 5. A List (Difficulty *******)
+// 4. A List (Difficulty *******)
 var list = {
   value: 1,
   rest: {
@@ -140,11 +124,11 @@ input list, and nth, which takes a list and a number and returns the element at 
 or undefined when there is no such element.
 ==============================================================================================================
 var arrayToList = function(array) {
-	var rest = null;
+	var rest = null; // rest is initially null.
 	for (var i = array.length - 1; i >= 0; i--) {
-		rest = {value: array[i], rest: rest}
+		rest = {value: array[i], rest: rest}; // accumulating each object into rest.
 	}
-	return rest;
+	return rest; // return the accumulated rest.
 };
 
 // Time: 4 min
@@ -154,7 +138,7 @@ var arrayToList = function(array) {
 // Why was it wrong?: It was correct. But It's really confusing I need to review it.
 
 
-// 6. Mother-child age difference (Difficulty ******)
+// 5. Mother-child age difference (Difficulty ******)
 Using the example data set from this chapter, compute the average age difference between mothers and children
 (the age of the mother when the child is born).
 
@@ -182,7 +166,7 @@ ancestry.forEach(function(person) {
 ==============================================================================================================
 var differences = ancestry.filter(function(person){
 	return byName[person.mother] != null; // What we're passing is a predicate. Valid elem will be pushed.
-}).map(function(person){
+}).map(function(person){								// let's always use != for null check.
 	return person.born - byName[person.mother].born; // This is correct.
 });
 
@@ -195,7 +179,7 @@ console.log(average(differences)); // → 31.2
 // Why was it wrong?: Because I used `filter` as if I was using `each`. `filter` is actually very similar to `map`.
 
 
-// 7. Historical life expectancy (Difficulty *******)
+// 6. Historical life expectancy (Difficulty *******)
 Compute and output the average age of the people in the ancestry data set per century. A person is assigned
 to a century by taking their year of death, dividing it by 100, and rounding it up, as in
 Math.ceil(person.died / 100).
@@ -205,16 +189,18 @@ function average(array) {
   return array.reduce(plus) / array.length;
 }
 ==============================================================================================================
-var groupBy = function(array, groupOf) {
+var groupBy = function(array, groupOf) { // groupOf is a callback function
 	var groups = {};
+
 	array.forEach(function(element){
-		var groupName = groupOf(element); // set group name
-		if (groupName in groups) { // check if the group name exists in the group
-			groups[groupName].push(element);
+		var groupName = groupOf(element);  // store the element's groupName
+		if (groupName in groups) {				 // if a certain groupName exists `if (prop in obj)` to check a prop's existence
+			groups[groupName].push(element); // groups[groupName] is an array.
 		} else {
-			groups[groupName] = [element];
+			groups[groupName] = [element];	 // if groupName is undefined, initiate it with [element] (element in an array)
 		}
 	});
+
 	return groups;
 };
 
@@ -222,9 +208,9 @@ var byCentury = groupBy(ancestry, function(person){ // grouping person objects i
 	return Math.ceil(person.died / 100);
 });
 
-for (var century in byCentury) {
-	var ages = byCentury[century].map(function(person){ // mapping ages to an age variable.
-		return person.died - person.born; // computing and mapping age over person objects.
+for (var century in byCentury) { // displaying average ages
+	var ages = byCentury[century].map(function(person){ // mapping ages to the age variable.
+		return person.died - person.born; // computing and mapping ages over each person objects.
 	});
 	console.log(century + ": " + average(ages)); // logging average age for each century in byCentury object.
 }
@@ -236,14 +222,14 @@ for (var century in byCentury) {
 // Why was it wrong?: It's just hard.
 
 
-// 8. [EXTRA CREDIT] Why doesn`t the code below work? This is a function that should return an array of
+// 7. [EXTRA CREDIT] Why doesn`t the code below work? This is a function that should return an array of
 functions that console.log() each person`s name as a string when invoked. Fiddle with this function and inspect
 how it works, then try to fix it using a closure. Be prepared to explain to a partner how it worked before, and
 how it works now with a closure. (Noteworthy ******)
 var checkAttendanceFunc = function(nameArr){
   var resultArr = [];
   for(var i = 0; i < nameArr.length; i++){
-    resultArr.push(function(){ console.log('Is', nameArr[i], 'present?', i)});
+    resultArr.push(function(){ console.log('Is', nameArr[i], 'present?', i)}); // the anonymous function is not invoked.
   };
   return resultArr;
 };
@@ -257,7 +243,7 @@ var checkAttendanceFunc = function(nameArr){
   };
   
   for(var i = 0; i < nameArr.length; i++){
-    resultArr.push(callback(nameArr)); // callback should be passed an argument it should also return value
+    resultArr.push(callback(nameArr)); // now callback() is invoked and pushed, which effectively close i for each invocation.
   };
   return resultArr;
 };
@@ -266,10 +252,10 @@ var checkAttendanceFunc = function(nameArr){
 // Expected result: n/a 
 // Actual result  : n/a
 // Do I know it?: Yes, but it's hard to explain.
-// Why was it wrong?: It was correct.
+// Why was it wrong?: the anonymous function inside the loop is not invoked, which means i is not closed in a scope.
 
 
-// 9. Retrieve id, title, and a 150x200 box art url for every video (Difficulty ******) Create a query that
+// 8. Retrieve id, title, and a 150x200 box art url for every video (Difficulty ******) Create a query that
 selects {id, title, boxart} for every video in the MovieLists.
 ==============================================================================================================
 Array.prototype.concatAll = function() {
